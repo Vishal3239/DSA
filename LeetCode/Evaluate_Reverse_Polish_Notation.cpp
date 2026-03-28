7ab0ec6b66d32a3f6ab0e50a65c3ff284403ca02
Vishal3239 <vishalpatelvp2004@gmail.com>
2026-03-28 21:47:53 +0530

    Reverse polish notation

---
+++ LeetCode/Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isOperator(string s) {
+    return (s == "+" || s == "-" || s == "*" || s == "/");
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<string>arr(n);
+    for(int i=0;i<n;++i)
+    cin>>arr[i];
+    stack<int>st;
+    int a,b;
+    for(string t:arr){
+        if(t=="+"){
+            a=st.top();
+            st.pop();
+            b=st.top();
+            st.pop();
+            st.push(a+b);
+        }else if(t=="-"){
+            a=st.top();
+            st.pop();
+            b=st.top();
+            st.pop();
+            st.push(a-b);
+        }else if(t=="*"){
+            a=st.top();
+            st.pop();
+            b=st.top();
+            st.pop();
+            st.push(a*b);
+        }else if(t=="/"){
+            a=st.top();
+            st.pop();
+            b=st.top();
+            st.pop();
+            st.push(b/a);
+        }else{
+            st.push(stoi(t));
+        }
+        
+    }
+    int ans=st.top();
+    st.pop();
+    cout<<ans;
+
+}
