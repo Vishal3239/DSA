d7d0d65479bdc4feb31b483e7974abdfe160f702
Vishal3239 <vishalpatelvp2004@gmail.com>
2026-04-19 15:53:52 +0530

    updated logic

--- LeetCode/ReplaceElements_with_GreatestElement_on_RightSide.cpp
+++ LeetCode/ReplaceElements_with_GreatestElement_on_RightSide.cpp
@@ -1,22 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-  int n;
-  cin>>n;
-  vector<int>nums(n),ans;
-  for(int i=0;i<n;++i){
-    cin>>nums[i];
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n), ans;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+    int gretest = -1;
+    for (int i = n - 1; i >= 0; --i)
+    {
+        ans.push_back(gretest);
 
-  }
-  int gretest=-1;
-  for(int i=n-1;i>=0;--i){
-    ans.push_back(gretest);
-
-    gretest=max(gretest,nums[i]);
-  }
-  reverse(ans.begin(),ans.end());
-  for(auto x:ans)
-  cout<<x<<" ";
-  return 0;
+        gretest = max(gretest, nums[i]);
+    }
+    reverse(ans.begin(), ans.end());
+    for (auto x : ans)
+        cout << x << " ";
+    return 0;
 }
