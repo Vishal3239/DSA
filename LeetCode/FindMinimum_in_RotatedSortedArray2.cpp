ea2837fe119aefee1614dde0fc4a56cafbedf39e
Vishal3239 <vishalpatelvp2004@gmail.com>
2026-05-16 15:34:58 +0530

    Find Minimum in Rotated Sorted Array II

---
+++ LeetCode/FindMinimum_in_RotatedSortedArray2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int minNum(vector<int> &nums)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[right])
+        {
+            left = mid + 1;
+        }
+        else if (nums[mid] < nums[right])
+        {
+            right = mid;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    return nums[left];
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i)
+        cin >> nums[i];
+    cout << minNum(nums);
+
+    return 0;
+}
