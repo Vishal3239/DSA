7ca2d2aaa047dee93c15ad0029d02d67244e835e
Vishal3239 <vishalpatelvp2004@gmail.com>
2026-05-21 08:27:21 +0530

    TrappingRainWater.cpp

---
+++ LeetCode/TrappingRainWater.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int trap(vector<int>& height) {
+
+    stack<int> st;
+    int water = 0;
+
+    for(int i = 0; i < height.size(); i++) {
+
+        while(!st.empty() && height[i] > height[st.top()]) {
+
+            int mid = st.top();
+            st.pop();
+
+            if(st.empty())
+                break;
+
+            int left = st.top();
+
+            int width = i - left - 1;
+
+            int boundedHeight =
+                min(height[left], height[i]) - height[mid];
+
+            water += width * boundedHeight;
+        }
+
+        st.push(i);
+    }
+
+    return water;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> num(n);
+
+    for(int i=0;i<n;i++)
+        cin >> num[i];
+
+    cout << trap(num);
+}
